6ed899aae7c59fe7a5bea98f9d6e5abc6c9a94d4
caowenbing <2474399710@qq.com>
2022-07-26 17:06:39 +0800

    second commit

---
+++ src/main/java/com/seecen/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.seecen.git;
+
+/**
+ * @author cao19
+ * @version 1.0
+ * @date 2022-07-26 17:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
